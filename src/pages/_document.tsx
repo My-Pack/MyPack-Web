dff87685c0065afa985de6cccf483f525e26bf72
hanseulhee <3021062@gmail.com>
2023-03-08 12:12:40 +0900

    [#1] env: 파일 구조 세팅

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+function Document() {
+  return (
+    <Html lang="kr">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document;
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>MyPack</title>
+        <meta name="description" content="MyPack" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
+
+export default Home;
