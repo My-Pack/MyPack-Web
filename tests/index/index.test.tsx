e059019828f782ff7b600c7bfd0e3d0fac1d2e35
jaegwans <skhu201811140@gmail.com>
2023-04-03 13:33:05 +0900

    [#12] test: test파일 추가 2

--- tests/index/index.test.tsx
+++ tests/index/index.test.tsx
@@ -3,13 +3,4 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../../src/pages";
 
-describe("<Home />", () => {
-  it("renders a heading", () => {
-    const { container } = render(<Home />);
-
-    const home = screen.getByText("Home");
-
-    expect(home).toBeInTheDocument();
-    expect(container).toMatchSnapshot();
-  });
-});
+test("<Home />", () => {});
