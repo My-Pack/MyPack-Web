635a9b33178df0ecadd67b7c8ac333ddebf8fdd7
jaegwans <skhu201811140@gmail.com>
2023-04-21 10:26:23 +0900

    [#36] feat: 로그인 백 통신 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,6 +9,24 @@ import {
   CredentialResponse,
 } from "@react-oauth/google";
 import LoginBack from "src/components/LoginBack";
+import { instance } from "src/libs/api/api";
+
+//로그인 api
+
+// refrash api
+function onLoginSuccess(response: CredentialResponse) {
+  instance
+    .post("/api/v1/auth/google", response)
+    .then((res: any) => {
+      localStorage.setItem("accessToken", res.accessToken);
+      localStorage.setItem("refreshToken", res.refreshToken);
+    })
+    .catch((err: any) => {
+      console.log(err);
+      alert("로그인에 실패했습니다. 다시 시도해주세요.");
+      //추후 모달창 개발 이후 모달창으로 변경
+    });
+}
 
 function Login() {
   return (
@@ -31,9 +49,9 @@ function Login() {
           >
             <GoogleLogin
               onSuccess={(response: CredentialResponse) =>
-                console.log(response)
+                onLoginSuccess(response)
               }
-              onError={() => console.log("Error ")}
+              onError={() => alert("Error ")}
             />
           </GoogleOAuthProvider>
 
