17cfc6046b5182c853d963d7f92eafba16c28243
hanseulhee <3021062@gmail.com>
2023-04-25 23:04:46 +0900

    [#35] feat: introduce Card 컴포넌트
    # 태그
    
    ## feat: 기능을 구현한다. (새로운 컴포넌트나 로직 추가)
    
    ## style: 코드 스타일을 수정하거나 CSS, UI에 관련된 코드를 추가하거나 수정한다.
    
    ## fix: 버그를 수정한다.
    
    ## docs: 리드미를 수정한다.
    
    ## refactor: 기능이나 로직을 변경하지 않고 코드를 개선시킨다.
    
    ## env: directory 구조를 변경하거나 파일명, directory 명을 변경하거나 라이브러리를 추가하거나 기타 config 설정을 할 때 사용한다.
    
    # 커밋 내용
    
    ## 끝에 마침표 넣지 않는다.
    
    ## 문장 형태 X
    
    ## 한글로 작성한다.

---
+++ src/components/Introduce/Card/index.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+interface IProps {
+  title: string;
+  subTitle: string;
+  component: JSX.Element;
+}
+
+function Card({ title, subTitle, component }: IProps) {
+  return (
+    <StyledWrapper>
+      <StyledTitleWrapper>
+        <StyledTitleInWrapper>
+          <StyledTitle>{title}</StyledTitle>
+          <StyledSubTitle>{subTitle}</StyledSubTitle>
+        </StyledTitleInWrapper>
+      </StyledTitleWrapper>
+      {component}
+    </StyledWrapper>
+  );
+}
+
+export default Card;
+
+const StyledWrapper = styled.div`
+  border-radius: ${({ theme }) => theme.borderRadius.uiCard};
+  background: linear-gradient(
+    225deg,
+    rgba(255, 255, 255, 0.2) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  width: auto;
+  cursor: pointer;
+`;
+
+const StyledTitleWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  height: min-content;
+
+  padding: 2.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+const StyledTitleInWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: min-content;
+
+  gap: 0.625rem;
+`;
+
+const StyledTitle = styled.span`
+  font-size: 1.7rem;
+  font-weight: ${({ theme }) => theme.fontWeight.semibold};
+`;
+
+const StyledSubTitle = styled.p`
+  color: ${({ theme }) => theme.color.grey500};
+  font-size: 1.05rem;
+  font-weight: ${({ theme }) => theme.fontWeight.light};
+`;
