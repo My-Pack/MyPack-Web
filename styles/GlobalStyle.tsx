d085b11357eaac6961ef4e27cdcb2b7ad9d38a85
hanseulhee <3021062@gmail.com>
2023-04-30 15:48:54 +0900

    [#35] style: default style 수정
    # 태그
    
    ## feat: 기능을 구현한다. (새로운 컴포넌트나 로직 추가)
    
    ## style: 코드 스타일을 수정하거나 CSS, UI에 관련된 코드를 추가하거나 수정한다.
    
    ## fix: 버그를 수정한다.
    
    ## docs: 리드미를 수정한다.
    
    ## refactor: 기능이나 로직을 변경하지 않고 코드를 개선시킨다.
    
    ## env: directory 구조를 변경하거나 파일명, directory 명을 변경하거나 라이브러리를 추가하거나 기타 config 설정을 할 때 사용한다.
    
    # 커밋 내용
    
    ## 끝에 마침표 넣지 않는다.
    
    ## 문장 형태 X
    
    ## 한글로 작성한다.

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -1,16 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    *{
+  * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         font-family: "Noto Sans KR", sans-serif;
-    }
+  }
 
-    html, body {
+  html, body {
     background-color: ${({ theme }) => theme.color.background};
     color: ${({ theme }) => theme.color.white};
   }
+
+  a {
+    all: unset;
+  }
 `;
+
 export default GlobalStyle;
