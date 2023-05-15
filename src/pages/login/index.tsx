c5565b05eecee25420f0bf43dcab0120f45e455b
jaegwans <skhu201811140@gmail.com>
2023-05-15 09:34:11 +0900

    [#48] refactor: 로직 분리

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -11,24 +11,24 @@ import {
 import LoginBack from "src/components/LoginBack";
 import { instance } from "src/libs/api/api";
 import { useRouter } from "next/router";
-
+import onHandleGoogleLogin from "src/hooks/api/onHandleGoogleLogin";
 //로그인 api
 
 // refrash api
-function onLoginSuccess(response: CredentialResponse, route: any) {
-  instance
-    .post("/api/v1/auth/google", response)
-    .then((res: any) => {
-      localStorage.setItem("accessToken", res.accessToken);
-      localStorage.setItem("refreshToken", res.refreshToken);
-      route.push("/");
-    })
-    .catch((err: any) => {
-      console.log(err);
-      alert("로그인에 실패했습니다. 다시 시도해주세요.");
-      //추후 모달창 개발 이후 모달창으로 변경
-    });
-}
+// function onLoginSuccess(response: CredentialResponse, route: any) {
+//   instance
+//     .post("/api/v1/auth/google", response)
+//     .then((res: any) => {
+//       localStorage.setItem("accessToken", res.accessToken);
+//       localStorage.setItem("refreshToken", res.refreshToken);
+//       route.push("/");
+//     })
+//     .catch((err: any) => {
+//       console.log(err);
+//       alert("로그인에 실패했습니다. 다시 시도해주세요.");
+//       //추후 모달창 개발 이후 모달창으로 변경
+//     });
+// }
 
 function Login() {
   const route = useRouter();
@@ -52,7 +52,7 @@ function Login() {
           >
             <GoogleLogin
               onSuccess={(response: CredentialResponse) =>
-                onLoginSuccess(response, route)
+                onHandleGoogleLogin(response, route)
               }
               onError={() => alert("Error ")}
             />
