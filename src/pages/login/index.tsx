e0c373cea1c762a45e466ca135c297c06812e3b9
jaegwans <skhu201811140@gmail.com>
2023-05-15 15:31:42 +0900

    [#48] fix: 불필요 로직 정리

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -12,26 +12,10 @@ import LoginBack from "src/components/LoginBack";
 import { instance } from "src/libs/api/api";
 import { useRouter } from "next/router";
 import onHandleGoogleLogin from "src/hooks/api/onHandleGoogleLogin";
-//로그인 api
-
-// refrash api
-// function onLoginSuccess(response: CredentialResponse, route: any) {
-//   instance
-//     .post("/api/v1/auth/google", response)
-//     .then((res: any) => {
-//       localStorage.setItem("accessToken", res.accessToken);
-//       localStorage.setItem("refreshToken", res.refreshToken);
-//       route.push("/");
-//     })
-//     .catch((err: any) => {
-//       console.log(err);
-//       alert("로그인에 실패했습니다. 다시 시도해주세요.");
-//       //추후 모달창 개발 이후 모달창으로 변경
-//     });
-// }
 
 function Login() {
   const route = useRouter();
+
   return (
     <div>
       <LoginBack />
