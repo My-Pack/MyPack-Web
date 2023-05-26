5d85c8f6976b6f014dc225f775971edfc72255ca
hanseulhee <3021062@gmail.com>
2023-05-26 16:12:58 +0900

    [#66] feat: howTo component 개발
    # 태그
    
    ## feat: 기능을 구현한다. (새로운 컴포넌트나 로직 추가)
    
    ## style: 코드 스타일을 수정하거나 CSS, UI에 관련된 코드를 추가하거나 수정한다.
    
    ## fix: 버그를 수정한다.
    
    ## docs: 리드미를 수정한다.
    
    ## refactor: 기능이나 로직을 변경하지 않고 코드를 개선시킨다.
    
    ## env: directory 구조를 변경하거나 파일명, directory 명을 변경하거나 라이브러리를 추가하거나 기타 config 설정을 할 때 사용한다.
    
    # 커밋 내용
    
    ## 끝에 마침표 넣지 않는다.
    
    ## 문장 형태 X
    
    ## 한글로 작성한다.

---
+++ src/components/Card/Design/HowTo.tsx
@@ -0,0 +1,144 @@
+import { MouseEvent } from "react";
+import { howTo } from "src/constants/howto";
+import styled from "styled-components";
+
+interface IProps {
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+function HowTo({ onClick }: IProps) {
+  return (
+    <StyledWrapper>
+      <StyledExitBtn onClick={onClick}>X</StyledExitBtn>
+      <StyledItemWrapper>
+        <StyledSizeWrapper>
+          <StyledItemInWrapper>
+            <StyledTitle>HowTo</StyledTitle>
+            <ol>
+              {howTo.map((data) => (
+                <li>
+                  <div>{data.num}</div>
+                  {data.content}
+                </li>
+              ))}
+            </ol>
+
+            <p>
+              시즌 별 테마 프레임은 곧 출시될 예정입니다. 이미지로 저장하고
+              싶으신 분은 <b>로그인 후</b> 마이페이지에서 카드를 클릭하면 이미지
+              저장 및 공유버튼이 나오게 됩니다.
+            </p>
+          </StyledItemInWrapper>
+        </StyledSizeWrapper>
+      </StyledItemWrapper>
+    </StyledWrapper>
+  );
+}
+
+export default HowTo;
+
+const StyledWrapper = styled.div`
+  position: fixed;
+  display: block;
+  width: 100vw;
+  height: 100vh;
+  padding: 0 7rem;
+
+  background-color: rgba(0, 0, 0, 0.85);
+  backdrop-filter: blur(5px);
+  color: ${({ theme }) => theme.color.white};
+
+  z-index: 1001;
+  overflow: auto;
+`;
+
+const StyledItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledSizeWrapper = styled.div`
+  height: 100%;
+  margin: 0px auto;
+`;
+
+const StyledItemInWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin-top: 1.8rem;
+
+  ol,
+  li {
+    margin-bottom: 2.5rem;
+    font-size: 1.5rem;
+  }
+
+  li {
+    letter-spacing: 1px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+
+    div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-left: 1px;
+      padding-bottom: 0.5px;
+      width: 2.5rem;
+      height: 2.5rem;
+      border-radius: 50%;
+
+      border: 4px solid ${({ theme }) => theme.color.neonPink};
+      color: ${({ theme }) => theme.color.neonPink};
+      font-size: 1.4rem;
+      font-weight: ${({ theme }) => theme.fontWeight.bold};
+      :hover {
+        cursor: pointer;
+      }
+    }
+  }
+  p {
+    font-size: 1.4rem;
+    line-height: 2rem;
+    margin: 2rem 0;
+
+    b {
+      color: ${({ theme }) => theme.color.neonMint};
+    }
+  }
+`;
+
+const StyledTitle = styled.span`
+  font-size: 3rem;
+  font-weight: ${({ theme }) => theme.fontWeight.semibold};
+  font-style: italic;
+  margin-bottom: 3rem;
+  border-bottom: 5px solid ${({ theme }) => theme.color.neonMint};
+  width: fit-content;
+`;
+
+const StyledExitBtn = styled.button`
+  all: unset;
+  position: absolute;
+  left: 2rem;
+  top: 1rem;
+
+  font-size: 3.5rem;
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  color: ${({ theme }) => theme.color.grey100};
+  cursor: pointer;
+
+  :hover {
+    color: ${({ theme }) => theme.color.buttonHoverBlack};
+    transition: 0.4s;
+  }
+`;
