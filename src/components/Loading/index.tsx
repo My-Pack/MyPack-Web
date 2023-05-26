e9dbf65f92b2d3b3b7414459364e7d4352c635be
jaegwans <skhu201811140@gmail.com>
2023-05-26 09:59:05 +0900

    [#55] fix:loading 컴포넌트 추가

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import LottieWrapper from "../Common/LottieWrapper";
+import styled from "styled-components";
+import lottieData from "src/assets/lottieJSON/loading.json";
 
 function Loading() {
-  return <div>로오딩</div>;
+  return (
+    <StyledLoading>
+      <LottieWrapper lottieData={lottieData} />
+    </StyledLoading>
+  );
 }
 
 export default Loading;
+
+const StyledLoading = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 10rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
