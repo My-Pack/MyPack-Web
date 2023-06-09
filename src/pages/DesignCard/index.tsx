30f4d22f2cc33bea073a4e14b94b63f99d32a9e4
hanseulhee <3021062@gmail.com>
2023-06-09 14:38:27 +0900

    [#77] fix: error 수정
    # 태그
    
    ## feat: 기능을 구현한다. (새로운 컴포넌트나 로직 추가)
    
    ## style: 코드 스타일을 수정하거나 CSS, UI에 관련된 코드를 추가하거나 수정한다.
    
    ## fix: 버그를 수정한다.
    
    ## docs: 리드미를 수정한다.
    
    ## refactor: 기능이나 로직을 변경하지 않고 코드를 개선시킨다.
    
    ## env: directory 구조를 변경하거나 파일명, directory 명을 변경하거나 라이브러리를 추가하거나 기타 config 설정을 할 때 사용한다.
    
    # 커밋 내용
    
    ## 끝에 마침표 넣지 않는다.
    
    ## 문장 형태 X
    
    ## 한글로 작성한다.

--- src/pages/DesignCard/index.tsx
+++ src/pages/DesignCard/index.tsx
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { instance } from "src/libs/api/api";
 import { useRouter } from "next/router";
 import { ChangeEvent, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -11,6 +9,7 @@ import HowTo from "src/components/Card/Design/HowTo";
 import PreviewCard from "src/components/Card/Design/PreviewCard";
 import Title from "src/components/Form/Title";
 import Nav from "src/components/Nav";
+import { instance } from "src/libs/api/api";
 import styled from "styled-components";
 
 function DesignCard() {
@@ -147,7 +146,6 @@ function DesignCard() {
             onClick={() => setIsClick(!isClick)}
             active={isClick ? true : false}
             title={title}
-            subTitle={subTitle}
             content={summary}
             color={theme}
             img={previewImage}
